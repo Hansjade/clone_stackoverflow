78fb4fd84e11158e682a3c296151c451d84c7978
easyearn77 <lju8702@gmail.com>
2022-08-24 17:22:46 +0900

    init spring file

---
+++ clone_stackoverflow/src/test/java/seb_39/pre_36/clone_stackoverflow/CloneStackoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package seb_39.pre_36.clone_stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneStackoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
